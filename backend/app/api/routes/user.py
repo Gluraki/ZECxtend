d9ae89a0158f732aa7e5b3187155dcf88818b8a3
Gluraki <gluraki.spamm@gmail.com>
2025-11-11 17:25:04 +0100

    scraping user creation stuff for now

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -3,6 +3,9 @@ import requests
 from app.models.user import User
 #from app.core.auth import get_current_user
 
+#Komplet WIP nix geht hier
+#sammlung an zeug das es gehen würde
+
 KC_ADMIN_URL = "http://localhost:8090/admin/realms/myapp/users"
 KC_ADMIN_TOKEN = "YOUR_ADMIN_ACCESS_TOKEN" 
 
@@ -43,3 +46,22 @@ def create_user(username: str, email: str, password: str):
         raise HTTPException(status_code=409, detail="User already exists")
     else:
         raise HTTPException(status_code=response.status_code, detail=response.text)
+    
+
+"""from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database.session import get_db
+from app.models.user import User
+#from app.services.keycloak import oauth2_scheme, decode_keycloak_token
+
+# da fehlt error handling ist für jetzt aber so schneller
+def get_current_user(token: str = Depends(oauth2_scheme),db: Session = Depends(get_db)):
+    payload = decode_keycloak_token(token)
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+"""
