ccca296d430c07d33dd8fd97268507cab8043b4e
Gluraki <gluraki.spamm@gmail.com>
2025-12-26 00:53:48 +0100

    Folder structure for fastapi microservices
    folder structure created with dockerfiles and a really basic dockercompose

---
+++ zec_api/services/attempt-service/app/api/main.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+#from app.api.routes import team, driver
+
+api_router = APIRouter()
+
+#api_router.include_router(team.router, prefix="/team", tags=["teams"])
---
+++ zec_api/services/team-service/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.main import api_router
+from app.core.config import settings
+from sqlalchemy.orm import Session
+from app.database.session import engine, Base
+
+def cstm_generate_unique_id(route: APIRoute) -> str:
+    if route.tags and len(route.tags) > 0:
+        return f"{route.tags[0]}-{route.name}"
+    else:
+        return f"untagged-{route.name}"
+
+app = FastAPI(
+    title="Team Service API",
+    openapi_url=f"{settings.API_STR}/openapi.json",
+    generate_unique_id_function=cstm_generate_unique_id,
+)
+
+app.include_router(api_router, prefix=settings.API_STR)
+Base.metadata.create_all(bind=engine)
