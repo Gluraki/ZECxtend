57192d77edad4b81617765da5d66b84346dfd31b
Gluraki <gluraki.spamm@gmail.com>
2025-12-28 16:35:32 +0100

    implementet the event based score creation
    while doing that realised calculation might be completly wrong

--- zec_api/services/score-service/app/crud/score.py
+++ zec_api/services/score-service/app/crud/score.py
@@ -8,14 +8,13 @@ from datetime import datetime
 CHALLENGE_URL = settings.CHALLENGE_SERVICE_URL
 ATTEMPT_URL = settings.ATTEMPT_SERVICE_URL
 TEAM_URL = settings.TEAM_SERVICE_URL
-DRIVER_URL = settings.DRIVER_SERVICE_URL
 
 score_processors = {}
 
 def calculate_f_pm(db_attempt):
-    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": db_attempt.challenge_id}).json()
+    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": db_attempt["challenge_id"]}).json()
     team = requests.get(f"{TEAM_URL}/api/teams/{fastest_attempt['team_id']}").json()
-    driver = requests.get(f"{DRIVER_URL}/api/drivers/{fastest_attempt['driver_id']}").json()
+    driver = requests.get(f"{TEAM_URL}/api/drivers/{fastest_attempt['driver_id']}").json()
     f_pm = (team["vehicle_weight"] + driver["weight"]) / team["mean_power"]
     return f_pm
 
@@ -38,17 +37,19 @@ def handle_skidpad(db_attempt):
 def handle_acceleration(db_attempt):
     f_pm_best_team = calculate_f_pm(db_attempt)
     f_points = 50 - f_pm_best_team / 20
-    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast", params={"challenge_id": db_attempt.challenge_id}).json()
+    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": db_attempt["challenge_id"]}).json()
     t_min = datetime.strptime(fastest_attempt["end_time"], "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(fastest_attempt["start_time"], "%Y-%m-%dT%H:%M:%S.%f")
-    fastest_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/per-team", params={"team_id": db_attempt.team_id, "challenge_id": db_attempt.challenge_id}).json()
+    t_min_seconds = t_min.total_seconds() 
+    fastest_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/per-team", params={"team_id": db_attempt["team_id"], "challenge_id": db_attempt["challenge_id"]}).json()
     t_team = datetime.strptime(fastest_attempt_team["end_time"], "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(fastest_attempt_team["start_time"], "%Y-%m-%dT%H:%M:%S.%f")
+    t_team_seconds = t_team.total_seconds()
     f_pm = calculate_f_pm(db_attempt)
-    score = f_points * t_min / t_team + f_pm / 20
+    score = f_points * (t_min_seconds / t_team_seconds) + f_pm / 20
     return score
 
 @register_score_processor("Slalom")
 def handle_slalom(db_attempt):
-    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast", params={"challenge_id": db_attempt.challenge_id}).json()
+    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": db_attempt["challenge_id"]}).json()
     t_min = datetime.strptime(fastest_attempt["end_time"], "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(fastest_attempt["start_time"], "%Y-%m-%dT%H:%M:%S.%f")
     fastest_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/per-team", params={"team_id": db_attempt.team_id, "challenge_id": db_attempt.challenge_id}).json()
     t_team = fastest_attempt_team["end_time"] - fastest_attempt_team["start_time"]
@@ -57,15 +58,15 @@ def handle_slalom(db_attempt):
 @register_score_processor("Endurance")
 def handle_endurance(db_attempt):
     # Time score
-    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast", params={"challenge_id": db_attempt.challenge_id}).json()
+    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": db_attempt["challenge_id"]}).json()
     t_min = datetime.strptime(fastest_attempt["end_time"], "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(fastest_attempt["start_time"], "%Y-%m-%dT%H:%M:%S.%f")
-    fastest_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/per-team", params={"team_id": db_attempt.team_id, "challenge_id": db_attempt.challenge_id}).json()
+    fastest_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/per-team", params={"team_id": db_attempt["team_id"], "challenge_id": db_attempt.challenge_id}).json()
     t_team = datetime.strptime(fastest_attempt_team["end_time"], "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(fastest_attempt_team["start_time"], "%Y-%m-%dT%H:%M:%S.%f")
     time_score = 75 * t_min / t_team
     # Energy score
-    least_energy_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/least-energy",params={"challenge_id": db_attempt.challenge_id}).json()
+    least_energy_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/least-energy",params={"challenge_id": db_attempt["challenge_id"]}).json()
     e_min = least_energy_attempt["energy_used"]
-    least_energy_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/least-energy/per-team",params={"team_id": db_attempt.team_id, "challenge_id": db_attempt.challenge_id}).json()
+    least_energy_attempt_team = requests.get(f"{ATTEMPT_URL}/api/attempts/least-energy/per-team",params={"team_id": db_attempt["team_id"], "challenge_id": db_attempt["challenge_id"]}).json()
     e_team = least_energy_attempt_team["energy_used"]
     energy_score = 175 * e_min / e_team
     return time_score + energy_score
@@ -75,6 +76,7 @@ def create_score(*, db: SessionDep, score: ScoreCreate):
     challenge = requests.get(f"{CHALLENGE_URL}/api/challenges/{db_attempt['challenge_id']}", params={"challenge_id": db_attempt["challenge_id"]}).json()
     if processor := score_processors.get(challenge["name"]):
         score_value = processor(db_attempt)
+    recalculate_acceleration_scores(db=db, challenge_id=db_attempt["challenge_id"])
     db_score = Score(
         attempt_id=score.attempt_id,
         challenge_id=db_attempt["challenge_id"],
@@ -103,3 +105,24 @@ def get_scores(*, db: SessionDep):
 
 def get_score_for_attempt(*, db: SessionDep, attempt_id: int):
     return db.query(Score).filter(Score.attempt_id == attempt_id).first()
+
+def recalculate_acceleration_scores(*, db: SessionDep, challenge_id: int):
+    challenge_response = requests.get(f"{CHALLENGE_URL}/api/challenges/{challenge_id}")
+    challenge = challenge_response.json()
+    if challenge["name"] != "Acceleration":
+        return False
+    fastest_attempt_id = None
+    fastest_attempt = requests.get(f"{ATTEMPT_URL}/api/attempts/fast/", params={"challenge_id": challenge_id}).json()
+    if fastest_attempt["id"] != fastest_attempt_id:
+        fastest_attempt_id = fastest_attempt["id"]
+        attempts_response = requests.get(f"{ATTEMPT_URL}/api/attempts/", params={"challenge_id": challenge_id})
+        attempts = attempts_response.json()
+        for attempt in attempts:
+            existing_score = db.query(Score).filter(Score.attempt_id == attempt["id"]).first()
+            if existing_score:
+                score_value = handle_acceleration(attempt)
+                existing_score.value = score_value
+        
+        db.commit()
+        return True
+
