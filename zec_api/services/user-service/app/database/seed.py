beb5902fcbeba88409a7294bfc83d6005507a47a
Gluraki <gluraki.spamm@gmail.com>
2026-02-16 18:30:38 +0100

    add admin role to the created user

--- zec_api/services/user-service/app/database/seed.py
+++ zec_api/services/user-service/app/database/seed.py
@@ -1,6 +1,6 @@
 from app.database.dependency import SessionDep
 from app.schemas.user import CreateUserKC
-from app.crud.user import create_user
+from app.crud.user import create_user, add_roles_to_user
 from app.models.user import User
 
 def seed_user(db: SessionDep):
@@ -11,4 +11,9 @@ def seed_user(db: SessionDep):
         exists = db.query(User).filter(User.username == user_data["username"]).first()
         
         if not exists:
-            create_user(db, CreateUserKC(**user_data))
+            db_user = create_user(db, CreateUserKC(**user_data))
+            if db_user:
+                try:
+                    add_roles_to_user(db_user.kc_id, ["ADMIN"])
+                except Exception as e:
+                    raise Exception(f"Failed to add roles to user {db_user.username} after creation: {e}")
