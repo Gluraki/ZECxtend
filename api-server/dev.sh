80c03a6e45e360a1a89c6138b7861dd73fd456c9
Gluraki <gluraki.spamm@gmail.com>
2026-04-22 09:23:36 +0200

    api-server: fix authentik setup in the docker-compose

--- api-server/dev.sh
+++ api-server/dev.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+set -e
+
+# App database
 export POSTGRES_USER=zec
 export POSTGRES_PASSWORD=changeme
 export POSTGRES_DB=zecxtend
+
+# Authentik database
+export PG_DB=authentik
+export PG_USER=authentik
 export PG_PASS=changeme
+
+# Authentik config
 export AUTHENTIK_SECRET_KEY=changeme-generate-a-real-one
+export AUTHENTIK_BOOTSTRAP_EMAIL=admin@localhost
+export AUTHENTIK_BOOTSTRAP_PASSWORD=changeme
+export AUTHENTIK_BOOTSTRAP_TOKEN=changeme-bootstrap-token
 
 docker compose up --build -d
