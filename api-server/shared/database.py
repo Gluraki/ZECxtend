36ba0a8523ac425d07eda14b1475521b10544d3f
Gluraki <gluraki.spamm@gmail.com>
2026-05-01 16:39:08 +0200

    api-server:  add old deps from auth-service

--- api-server/shared/database.py
+++ api-server/shared/database.py
@@ -42,3 +42,44 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
             raise
 
 SessionDep = Annotated[AsyncSession, Depends(get_db)]
+
+""""
+def get_current_user(payload: dict = Depends(decode_keycloak_token)):
+    roles = extract_roles_from_payload(payload)
+    if not roles:
+        raise exception.InsufficientPermissions()
+    return {
+        "sub": payload.get("sub"),
+        "email": payload.get("email"),
+        "username": payload.get("preferred_username"),
+        "roles": roles,
+        "token_payload": payload
+    }
+ROLE_HIERARCHY = {
+    UserRole.VIEWER: {
+        UserRole.VIEWER,
+        UserRole.TEAM_LEAD,
+        UserRole.ADMIN,
+    },
+    UserRole.TEAM_LEAD: {
+        UserRole.TEAM_LEAD,
+        UserRole.ADMIN,
+    },
+    UserRole.ADMIN: {
+        UserRole.ADMIN,
+    },
+}
+
+def require_role(required_role: UserRole):
+    def role_checker(user: CurrentUser):
+        user_roles = {UserRole(role) for role in user["roles"]}
+        allowed_roles = ROLE_HIERARCHY[required_role]
+        if user_roles.isdisjoint(allowed_roles):
+            raise exception.InsufficientPermissions
+        return user
+    return role_checker
+
+AdminUser = Annotated[dict, Depends(require_role(UserRole.ADMIN))]
+TeamLeadUser = Annotated[dict, Depends(require_role(UserRole.TEAM_LEAD))]
+ViewerUser = Annotated[dict, Depends(require_role(UserRole.VIEWER))]
+"""
