0cafd28f62d42c7f3e892023d6f09af1f5ffec2e
Gluraki <gluraki.spamm@gmail.com>
2026-05-27 21:56:21 +0200

    api-server: remove legacy files

--- api-server/migrations/versions/82ef9d99882f_add_password_hash.py
+++ api-server/migrations/versions/82ef9d99882f_add_password_hash.py
@@ -7,9 +7,8 @@ Create Date: 2026-05-27 13:31:33.047127
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '82ef9d99882f'
--- api-server/migrations/versions/9896c8c74d29_update_user.py
+++ api-server/migrations/versions/9896c8c74d29_update_user.py
@@ -7,8 +7,8 @@ Create Date: 2026-05-27 13:12:08.014903
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
