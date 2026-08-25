faa162847aacfe0e9fef3b422a4b6762f90a48f9
Gluraki <gluraki.spamm@gmail.com>
2026-08-25 16:18:05 +0200

    api-server: fix scripts

--- api-server/scripts/dev-start.sh
+++ api-server/scripts/dev-start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eo pipefail
 
 source env.sh
--- api-server/scripts/env.sh
+++ api-server/scripts/env.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export POSTGRES_USER=zec
 export POSTGRES_PASSWORD=changeme
 export POSTGRES_DB=zecxtend
