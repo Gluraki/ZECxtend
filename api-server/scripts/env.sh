d7fb3bb2bf56399606654cb2217391e72d97a285
Gluraki <gluraki.spamm@gmail.com>
2026-09-05 10:16:52 +0200

    api-server: add CORS to gateway

--- api-server/scripts/env.sh
+++ api-server/scripts/env.sh
@@ -3,6 +3,8 @@ export POSTGRES_USER=zec
 export POSTGRES_PASSWORD=changeme
 export POSTGRES_DB=zecxtend
 
+export CORS_ALLOWED_ORIGINS=
+
 export DOCS_ENABLED=true
 export DOCS_USERNAME=admin
 export DOCS_PASSWORD=changeme
